acfb41a5edf6dc420d6978b2d613d9857c7deeb2
Developer <23010584@st.phenikaa-uni.edu.vn>
2026-01-28 16:37:22 +0700

    tạo route cho categories, products, sizes, orders

--- Code/webdouong/routes/web.php
+++ Code/webdouong/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SizeController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Routes cho Categories (CRUD)
+Route::resource('categories', CategoryController::class);
+
+// Routes cho Products (CRUD)
+Route::resource('products', ProductController::class);
+
+// Routes cho Sizes (CRUD)
+Route::resource('sizes', SizeController::class);
+
+// Routes cho Orders (CRUD + Xem đơn)
+Route::resource('orders', OrderController::class);
+
