681e083482d4fd53fe9bcf6c214963027c91c4d1
Developer <23010584@st.phenikaa-uni.edu.vn>
2026-01-28 17:53:08 +0700

    cài package breeze (login, registe views)

--- Code/webdouong/routes/web.php
+++ Code/webdouong/routes/web.php
@@ -1,24 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SizeController;
-use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Routes cho Categories (CRUD)
-Route::resource('categories', CategoryController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Routes cho Products (CRUD)
-Route::resource('products', ProductController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Routes cho Sizes (CRUD)
-Route::resource('sizes', SizeController::class);
-
-// Routes cho Orders (CRUD + Xem đơn)
-Route::resource('orders', OrderController::class);
-
+require __DIR__.'/auth.php';
